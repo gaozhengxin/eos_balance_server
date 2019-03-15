2248308733404d825bc38a2e608e8f0d8030d7b4
gaozhengxin <zhengxingao@live.cn>
2019-03-15 18:29:18 +0800

    add http server, delete binary file (wrongly included in previous commit)

---
+++ server/balance_server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"../dao"
+	"../transfer_tracker"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main () {
+	port := "1234"
+	path := "0.0.0.0:" + port
+	defer dao.Close()
+	go tracker.Run()
+	http.HandleFunc("/get_balance", GetBalance)
+	http.ListenAndServe(path, nil)
+	select{}
+}
+
+type Resp struct {
+	Code string `json:"code"`
+	Msg string `json:"Msg,omitempty"`
+	Balance string `json:"balance,omitempty"`
+}
+
+func GetBalance (writer http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	userKey, ok := request.Form["user_key"]
+	var result Resp
+	if !ok {
+		result.Code = "401"
+		result.Msg = "no user_key"
+	} else {
+		result.Code = "200"
+		result.Balance = dao.GetBalance(userKey[0])
+	}
+	if err := json.NewEncoder(writer).Encode(result); err != nil {
+		log.Fatal(err)
+	}
+}
