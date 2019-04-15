49aafe38b8146f75561b71f50f6c0b4c24056cea
gaozhengxin <zhengxingao@live.cn>
2019-04-15 14:56:53 +0800

    fix bug: userkey pattern incorrect

--- utils/userkey.go
+++ utils/userkey.go
@@ -3,7 +3,7 @@ package utils
 import "regexp"
 
 func IsUserKey (memo string) bool {
-	if ok, _ := regexp.Match("^d[1-5a-zA-Z_]{33}$", []byte(memo)); ok {
+	if ok, _ := regexp.Match("^d[1-5a-z]{33}$", []byte(memo)); ok {
 		return true
 	}
 	return false
